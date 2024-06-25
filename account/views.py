1242cf880fafc173686ca214e01414d8068aaafa
Oluwatosin <kingosenitosin@gmail.com>
2024-06-24 23:28:18 -0500

    changed redirect of user registration

--- account/views.py
+++ account/views.py
@@ -16,7 +16,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse("User Succesfully Registered")
+            return redirect("login")
     return render(request, "account/register.html", {
             "registered_form":form
         })
