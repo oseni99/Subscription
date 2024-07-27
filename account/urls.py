f6730b12fcc38c61be2fdd0f2b403b22bfd5bb2f
Oluwatosin <kingosenitosin@gmail.com>
2024-07-26 19:12:13 -0500

    Chore: Added Password Reset Functionality

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,44 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path("",views.home_page,name="home"),
+    path("", views.home_page, name="home"),
     path("register", views.register, name="register"),
     path("login", views.my_login, name="login"),
     path("user-logout", views.user_logout, name="user_logout"),
+    # Password reset urls management
+    # 1 - Request password reset email
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="account/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+    # 2 - Password reset email sent success mssg
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="account/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    # 3 - reset password itself page as we enter a new password
+    path(
+        "password_reset/confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="account/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    # 4 - password reset success mssg
+    path(
+        "password_reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="account/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
